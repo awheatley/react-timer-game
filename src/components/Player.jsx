969b5d6f4337eba7ffd36e34936dbb3341c1b204
Adam Wheatley <wheatley.adam@me.com>
2024-07-28 21:14:27 -0500

    wire up input to change player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerNameInput = useRef();
+
+  const [enteredPlayer, setEnteredPlayer] = useState('');
+  
+  function handleClick() {
+    setEnteredPlayer(playerNameInput.current.value);
+    playerNameInput.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayer ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameInput} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
