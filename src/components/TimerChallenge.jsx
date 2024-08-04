c7e18c13c5680b382a2e343228a53aec695a561c
Adam Wheatley <wheatley.adam@me.com>
2024-08-03 21:50:44 -0500

    add score to results modal

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -14,6 +14,10 @@ export default function TimerChallenge({ title, targetTime }) {
     dialog.current.open();
   }
 
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
   function handleStart() {
     timer.current = setInterval(() => {
       setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
@@ -27,7 +31,7 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
+      <ResultModal ref={dialog} result="lost" targetTime={targetTime} remainingTime={timeRemaining} onReset={handleReset} />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
